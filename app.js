8bce4f14ebfc58ef31681a7c01f7e2ada5a18413
hira <hquddus021@gmail.com>
2020-07-20 13:27:01 -0400

    db connected

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err));
+
 
 app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
